58df62f14b9f1818245cd29ce3443f11db2fbfc8
Dpartipilo <Dpartipilo@hotmail.com>
2018-12-01 14:13:58 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Navbar />
+        <Footer />
       </div>
     );
   }
 }
+
+const Navbar = () => (
+  <Router>
+    <div >
+      <nav className="navbar">
+        <Link to="/">Home</Link>
+        <Link to="/about/">About</Link>
+        <Link to="/users/">Users</Link>
+      </nav>
+
+      <Route path="/" exact component={Index} />
+      <Route path="/about/" component={About} />
+      <Route path="/users/" component={Users} />
+
+    </div>
+  </Router>
+)
+
+const Index = () => <h2>Home</h2>;
+const Footer = () => <div className="footer">
+  <h2>Footer</h2>
+</div>;
+const About = () => <div><h2 onClick={console.log('hola!')}>About us</h2></div>;
+const Users = () => <h2>Users</h2>;
 
 export default App;
