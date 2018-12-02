ce687688ed14285061713e116386cbf2dc4da0af
Dpartipilo <Dpartipilo@hotmail.com>
2018-12-02 22:55:48 +0000

    Added Galery link and component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import './App.css';
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -15,26 +15,34 @@ class App extends Component {
 
 const Navbar = () => (
   <Router>
-    <div >
+    <div>
       <nav className="navbar">
         <Link to="/">Home</Link>
         <Link to="/about/">About</Link>
         <Link to="/users/">Users</Link>
+        <Link to="/galery/">Galery</Link>
       </nav>
 
       <Route path="/" exact component={Index} />
       <Route path="/about/" component={About} />
       <Route path="/users/" component={Users} />
-
+      <Route path="/galery/" component={Galery} />
     </div>
   </Router>
-)
+);
 
 const Index = () => <h2>Home</h2>;
-const Footer = () => <div className="footer">
-  <h2>Footer test</h2>
-</div>;
-const About = () => <div><h2 onClick={console.log('hola!')}>About us</h2></div>;
+const Galery = () => <h2>Galery</h2>;
+const Footer = () => (
+  <div className="footer">
+    <h2>Footer test</h2>
+  </div>
+);
+const About = () => (
+  <div>
+    <h2 onClick={console.log("hola!")}>About us</h2>
+  </div>
+);
 const Users = () => <h2>Users</h2>;
 
 export default App;
