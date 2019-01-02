7430c448468f445c9bc7b2ed667eaa70ca07b9b2
Dpartipilo <Dpartipilo@hotmail.com>
2019-01-02 23:31:02 +0000

    Added comming events container

--- src/App.js
+++ src/App.js
@@ -47,6 +47,8 @@ const Navbar = () => (
 const Home = () => (
   <div>
     <Carousel />
+    <h1>Comming Events</h1>
+    <CommingEvents />
   </div>
 );
 
@@ -54,6 +56,23 @@ const Carousel = () => (
   <div className="container">
     <div className="carousel">
       <h2>Home</h2>
+    </div>
+  </div>
+);
+
+let aEvents = [
+  { title: "TestTitle", image: "testImage" },
+  { title: "TestTitle2", image: "testImage2" }
+];
+const CommingEvents = () => (
+  <div className="container">
+    <div className="commingEvents">
+      {aEvents.map(event => (
+        <div className="singleEvent">
+          <h1>{event.title}</h1>
+          <h1>{event.image}</h1>
+        </div>
+      ))}
     </div>
   </div>
 );
