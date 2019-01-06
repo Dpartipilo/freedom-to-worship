ee61cc8539fc7ddc954691df6c4d329623efcbf9
Dpartipilo <Dpartipilo@hotmail.com>
2019-01-06 12:52:38 +0000

    Added new components

---
+++ src/components/About.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const About = () => (
+  <div>
+    <h2 onClick={console.log("hola Amigos!!")}>About kkjavsasdasdasus</h2>
+  </div>
+);
+
+export default About;
---
+++ src/components/Galery.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Galery = () => <h2>Galery</h2>;
+
+export default Galery;
