563325df4de8f9952aa63f1f264c7edbabc64f5b
Dpartipilo <Dpartipilo@hotmail.com>
2019-01-13 20:05:36 +0000

    Removed CommingEvents from Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,9 @@
 import React from "react";
+import CommingEvents from "./CommingEvents";
 
 const Home = () => (
   <div>
     <Carousel />
-    <h1>Comming Events</h1>
     <CommingEvents />
   </div>
 );
@@ -16,21 +16,4 @@ const Carousel = () => (
   </div>
 );
 
-let aEvents = [
-  { title: "TestTitle", image: "testImage" },
-  { title: "TestTitle2", image: "testImage2" }
-];
-
-const CommingEvents = () => (
-  <div className="container">
-    <div className="commingEvents">
-      {aEvents.map(event => (
-        <div className="singleEvent">
-          <h1>{event.title}</h1>
-          <h1>{event.image}</h1>
-        </div>
-      ))}
-    </div>
-  </div>
-);
 export default Home;
