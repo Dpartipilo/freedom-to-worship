41c9fd9b8dbcf9012d2afcf184615593a977a937
Dpartipilo <Dpartipilo@hotmail.com>
2019-01-13 20:31:10 +0000

    Moved singleEvent Component to it's own component file

--- src/components/CommingEvents.js
+++ src/components/CommingEvents.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import SingleEvent from "./SingleEvent";
 
 class CommingEvents extends Component {
   constructor(props) {
@@ -7,18 +8,21 @@ class CommingEvents extends Component {
       aEvents: [
         {
           title: "Una noche de navidad!",
+          id: "una-noche-de-navidad",
           imageUrl:
             "https://static.wixstatic.com/media/b93c26360b3b49f59d94079ecaa9dc6f.jpg/v1/fit/w_681,h_454,al_c,q_80/file.webp",
           date: "Oct 15, 2018"
         },
         {
           title: "TestTitle2",
+          id: "test-title-2",
           imageUrl:
             "https://static.wixstatic.com/media/3e3391443ed346b7ae6740fdcd37aa68.jpg/v1/fit/w_681,h_454,al_c,q_80/file.webp",
           date: "Oct 15, 2018"
         },
         {
-          title: "TestTitle2",
+          title: "TestTitle3",
+          id: "test-title-3",
           imageUrl:
             "https://static.wixstatic.com/media/24352a95df9d4d0393d5115facf22c28.jpg/v1/fit/w_682,h_454,al_c,q_80/file.webp",
           date: "Oct 15, 2018"
@@ -38,15 +42,8 @@ class CommingEvents extends Component {
       <div className="container">
         <h1>Comming Events</h1>
         <div className="commingEvents">
-          {this.state.aEvents.map((event, i) => (
-            <div
-              style={{ backgroundImage: `url(${event.imageUrl})` }}
-              key={i}
-              className="singleEvent"
-            >
-              <p>{event.date}</p>
-              <h1>{event.title}</h1>
-            </div>
+          {this.state.aEvents.map(singleEvent => (
+            <SingleEvent {...singleEvent} />
           ))}
         </div>
       </div>
